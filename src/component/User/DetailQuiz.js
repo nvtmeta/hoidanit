85774fb697c99e263c72ebc5b91484d128c49483
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-02-02 12:23:28 +0700

    video83

--- src/component/User/DetailQuiz.js
+++ src/component/User/DetailQuiz.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { getQuizData } from "../../services/apiService";
-
+import _ from "lodash";
 const DetailQuiz = () => {
   const params = useParams();
   const quizId = params.id;
@@ -11,7 +11,30 @@ const DetailQuiz = () => {
 
   const fetchQuiz = async () => {
     const res = await getQuizData(quizId);
-    console.log(res);
+    if (res && res.EC === 0) {
+      console.log(res.DT);
+      let raw = res.DT;
+      let data = _.chain(raw)
+
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.questionDescription;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+          return { questionId: key, answers, questionDescription, image };
+        })
+        .value();
+      console.log(data);
+    }
   };
   return <div>scasc</div>;
 };
--- src/utils/axiosCustomise.js
+++ src/utils/axiosCustomise.js
@@ -11,8 +11,6 @@ NProgress.configure({ showSpinner: false, trickleSpeed: 100 });
 instance.interceptors.request.use(
   function (config) {
     const access_token = store?.getState()?.user?.account?.access_token;
-    console.log(store?.getState());
-    console.log(access_token);
     // config.headers["Authorization"] = "Bearer" + access_token; //error :))
     config.headers["Authorization"] = `Bearer ${access_token}`; //amazing!
     NProgress.start();
